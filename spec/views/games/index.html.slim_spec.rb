6d98a4cee54cf7061d7286ea716a468814143f76
Matt Farmer <mrfarmer777@gmail.com>
2024-09-28 06:14:18 -0400

    Add games index page

---
+++ spec/views/games/index.html.slim_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "games/index.html.slim", type: :view do
+  it 'lists all of the games' do
+    player1 = Player.create(name: 'foo')
+    player2 = Player.create(name: 'bar')
+    player3 = Player.create(name: 'baz')
+    assign(:games, [Game.create(players: [player1, player2]), Game.create(players: [player1,player3])])
+
+    render
+
+    expect(rendered).to match /foo/
+    expect(rendered).to match /bar/
+    expect(rendered).to match /baz/
+    expect(rendered).to have_selector('tr.game', count: 2)
+  end
+end
