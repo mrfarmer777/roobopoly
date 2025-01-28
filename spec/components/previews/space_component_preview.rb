760ac8bf1f70191145b945fad0935f1777289da8
Matt Farmer <mrfarmer777@gmail.com>
2025-01-27 20:01:53 -0600

    Fix spec broken by previous change

--- spec/components/previews/space_component_preview.rb
+++ spec/components/previews/space_component_preview.rb
@@ -4,7 +4,6 @@ class SpaceComponentPreview < ViewComponent::Preview
   def default
     render(SpaceComponent.new(name: 'Aardvark Boulevard',
                               position: 1,
-                              color: '#ff00ff',
                               player_names: ['Shackleford']))
   end
 end
